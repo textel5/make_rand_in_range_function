adeae76c3de4787640d46aedd01ac02682f6c5b3
Sergey Tokarev <sergey.tokarev.nsk@mail.ru>
2021-10-29 07:41:31 +0000

    added license notice

--- make_rand_in_range_function.h
+++ make_rand_in_range_function.h
@@ -1,3 +1,19 @@
+/*
+    make_rand_in_range_funtion makes a random random in needed range
+    Copyright (C) 2021  Sergey Tokarev
+....
+    This file is part of make_rand_in_range.
+    make_rand_in_range is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    listint is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with listint.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef MAKE_RAND_IN_RANGE_FUNCTION_SENTRY
 #define MAKE_RAND_IN_RANGE_FUNCTION_SENTRY
 
